ab1117fe45aff0f12b312126e820b86d0d8cd94d
bulletman24 <egues.telmo@gmail.com>
2022-03-30 22:58:15 +0100

    works, modifications left to do, added messages

--- usb_driver.c
+++ usb_driver.c
@@ -14,8 +14,8 @@
 #define USB_ID_PRODUCT	( 0xf003 ) /*Mcro for the ID of the product*/
 
 static int pr_probe (struct usb_interface *interface, const struct usb_device_id *id) {
-	//struct usb_host_interface *iface_desc = interface->cur_altsetting; //iface_desc is defined as a usb_host_interface structure which contains all the interface descriptors
-
+	struct usb_host_interface *iface_desc = interface->cur_altsetting; //iface_desc is defined as a usb_host_interface structure which contains all the interface descriptors
+	pr_info("bLength: 0x%x\n", iface_desc->desc.bLength);
 	dev_info(&interface->dev, "USB detected with Vendor ID: 0x%02x an Product ID: 0x%02x/n", id->idVendor, id->idProduct);
 	return 0;
 }
